3c0d71b548eede52fd09312ef5b8108c9c933139
Rokas Barasa <50622827+Rokasbarasa1@users.noreply.github.com>
2021-11-21 15:16:43 +0100

    Fix redis import

--- apis/movies.js
+++ apis/movies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {redisSet} = require("../middleware/redisMiddleware")
+// const {redisSet} = require("../middleware/redisMiddleware")
 const moviesService = require('../services/moviesService');
 
 /**
